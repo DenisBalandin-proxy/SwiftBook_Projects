21ba1f0a0a0b86593ba1cbf6507f4489c9588605
Denis Balandin <doublestar2013@yandex.ru>
2022-09-14 23:01:33 +0300

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Denis on 2022/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstView: UIView!
+    
+    @IBOutlet weak var secondView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
